2f7a6d4e6a8b020bbdb4f13316d0b72af6f9fec1
channss <chanmin.park@wantedlab.com>
2024-02-26 18:40:50 +0900

    local test

--- projects/laas/laas_semantic_search.py
+++ projects/laas/laas_semantic_search.py
@@ -1,26 +1,23 @@
 import boto3
 import requests
+import hashlib
+
 
 def get_parameter(key):
     ssm = boto3.client('ssm', region_name='ap-northeast-2')
     return ssm.get_parameter(Name=key, WithDecryption=True)['Parameter']['Value']
 
-# class Env(StageABC):
-#     _stage = {
-#         'nextweek': 'nw',
-#         'www': 'www',
-#     }
-#     base_url = {
-#         'nextweek': 'https://nextweek-api-laas.wanted.co.kr',
-#         'www': 'https://api-laas.wanted.co.kr',
-#     }
+
+def hashing(string):
+    return int.from_bytes(
+        hashlib.sha256(string.encode()).digest()[:4]
+    )
 
 
 def call_wanted_api(method, path, **kwargs):
     return requests.request(
         method=method,
-        # url=f"{Env.config('base_url')}{path}",
-        url=f"https://nextweek-api-laas.wanted.co.kr{path}",
+        url=f"https://dev-api-laas.wanted.co.kr{path}",
         headers={
             "project": "WANTED_DATA",
             "apiKey": WANTED_LAAS_API_KEY,
@@ -29,8 +26,16 @@ def call_wanted_api(method, path, **kwargs):
         **kwargs,
     )
 
+
 if __name__ == '__main__':
-    WANTED_LAAS_API_KEY = get_parameter('/DATA/PIPELINE/API_KEY/OPENAI')
+    WANTED_LAAS_API_KEY = get_parameter('/DATA/PIPELINE/API_KEY/OPENAI/DEV')
 
     doc_id = 88173997
-    res = call_wanted_api('GET', f"/api/document/wanted_data_catalog/{doc_id}")
+    # res = call_wanted_api('GET', f"/api/document/channtest/{1}")
+    collection_name = 'chann_test'
+    body = {
+        "dimension": 1536,
+        "embedding_model": "text-embedding-ada-002",
+        "service_type": "azure"
+    }
+    # res = call_wanted_api('POST', f'/api/collection/{collection_name}', json=body)
